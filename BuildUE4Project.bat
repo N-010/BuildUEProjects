eebd1ed2e5d00b88b36e8cc823f100e985711cbc
N-010 <chelsig96@gmail.com>
2021-02-02 12:51:18 +0300

    Add files via upload
    To run the script, you need to change the following variables:
    EnginePath
    ProjectPath
    BuildPath
    ProjectName

---
+++ BuildUE4Project.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set EnginePath=G:\UE_4.25\Engine\
+set ProjectPath=G:\Projects\TheWicked\
+set BuildPath=G:\Build
+set ProjectName=TheWicked
+set ProjectNameEditor=TheWickedEditor
+set UprojectPath=%ProjectPath%%ProjectName%.uproject
+set SlnPath=%ProjectPath%%ProjectName%.sln
+
+%EnginePath%Binaries\DotNET\UnrealBuildTool.exe -projectfiles -project=%UprojectPath% -game -rocket -progress
+
+call %EnginePath%Build\BatchFiles\Rebuild.bat %ProjectNameEditor% Win64 Development -Project=%UprojectPath% -WaitMutex -FromMsBuild
+
+%EnginePath%Build\BatchFiles\RunUAT.bat -ScriptsForProject=%UprojectPath% BuildCookRun -nocompileeditor -installed -nop4 -project=%UprojectPath% -cook -stage -archive -archivedirectory=%BuildPath% -package -ue4exe=%EnginePath%Binaries\Win64\UE4Editor-Cmd.exe -build -pak -prereqs -nodebuginfo -targetplatform=Win64 -target=%ProjectName% -clientconfig=Development -utf8output 
