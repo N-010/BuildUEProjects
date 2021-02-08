5c7bc3521f827f5ebd9af5b3ed92c32d9be035bd
N-010 <chelsig96@gmail.com>
2021-02-08 12:01:30 +0300

    Update BuildUE4Project.bat

--- BuildUE4Project.bat
+++ BuildUE4Project.bat
@@ -8,8 +8,8 @@ set ProjectNameEditor=%ProjectName%Editor
 set UprojectPath=%ProjectPath%%ProjectName%.uproject
 set SlnPath=%ProjectPath%%ProjectName%.sln
 
-%EnginePath%Binaries\DotNET\UnrealBuildTool.exe -projectfiles -project=%UprojectPath% -game -rocket -progress
+"%EnginePath%Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -project="%UprojectPath%" -game -rocket -progress
 
-call %EnginePath%Build\BatchFiles\Rebuild.bat %ProjectNameEditor% Win64 Development -Project=%UprojectPath% -WaitMutex -FromMsBuild
+call "%EnginePath%Build\BatchFiles\Rebuild.bat" "%ProjectNameEditor%" Win64 Development -Project="%UprojectPath%" -WaitMutex -FromMsBuild
 
-%EnginePath%Build\BatchFiles\RunUAT.bat -ScriptsForProject=%UprojectPath% BuildCookRun -nocompileeditor -installed -nop4 -project=%UprojectPath% -cook -stage -archive -archivedirectory=%BuildPath% -package -ue4exe=%EnginePath%Binaries\Win64\UE4Editor-Cmd.exe -build -pak -prereqs -nodebuginfo -targetplatform=Win64 -target=%ProjectName% -clientconfig=Development -utf8output 
+"%EnginePath%Build\BatchFiles\RunUAT.bat" -ScriptsForProject="%UprojectPath%" BuildCookRun -nocompileeditor -installed -nop4 -project="%UprojectPath%" -cook -stage -archive -archivedirectory="%BuildPath%" -package -ue4exe="%EnginePath%Binaries\Win64\UE4Editor-Cmd.exe" -build -pak -prereqs -nodebuginfo -targetplatform=Win64 -target="%ProjectName%" -clientconfig=Development -utf8output 
